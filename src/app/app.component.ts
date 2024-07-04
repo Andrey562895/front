906c3b8ab2d1b966568592ae27796e9347f273cc
Andrey562895 <andrey.77as@yandex.ru>
2024-07-04 15:38:26 +0300

    feat: add front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { DogsService } from './services/dogs.service';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'front';
+
+  constructor(private dogService: DogsService) { }
+  ngOnInit() {
+    this.dogService.getDogs().subscribe(data => {
+      console.log(data);
+    })
+  }
+
 }
